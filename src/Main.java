2ffcd674b4355beee70575c5e48d8aab3c16370a
Bara Abraham <barasec@protonmail.com>
2019-01-02 18:45:34 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// Name : Bara Adnan
-// BZU #ID : 1161357
-
 public class Main {
 
     public static void main(String[] args)
